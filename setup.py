1e05a89fc5edb73791227f61baa278954a945857
Myktarali <36340362+Myktarali@users.noreply.github.com>
2020-05-02 15:59:10 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Django',
+    version='',
+    packages=['carparts'],
+    url='',
+    license='',
+    author='MiCKee',
+    author_email='',
+    description=''
+)
